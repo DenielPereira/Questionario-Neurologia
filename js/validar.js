7a8c15f6b7ab07f72e5a17bc1c0dc5143e82c8ab
DENIEL SANTOS PEREIRA <deniel.pereira@sedefccr.local>
2018-04-27 17:48:33 -0300

    validação do campo email para cadastro

--- js/validar.js
+++ js/validar.js
@@ -1,14 +1,20 @@
-   function enviar() {
+function checkEmail(email)
+{
+    var aviso = document.getElementById("aviso");
+    var borda = document.getElementById("email");
+    var botao = document.getElementById("cadastrar");
 
-        if (document.getElementById('usuario').value == "") {
-            document.getElementById('usuario').setCustomValidity('Voce esqueceu de preencher esse campo');
-            document.getElementById('usuario').focus();
-            return false;
-        }
-
-        return true;
-    
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!re.test(email)){
+        aviso.innerHTML = "Isso nao se parece com um endereço de e-mail.";
+        borda.style.border = "1px groove #FF0000";
+        botao.style.background = "#757575";
+        botao.disabled = true;
+    }else{
+        aviso.innerHTML = "";
+        borda.style.border = "0";
+        botao.style.background = "#46087F";
+        botao.disabled = false;
     }
-
-
+}
 
