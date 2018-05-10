f4a21abe7fb05bf993e602587cd98613b77271b4
DENIEL SANTOS PEREIRA <deniel.pereira@sedefccr.local>
2018-05-10 17:38:41 -0300

    merdas feitas sem pensar

--- scripts/validar.js
+++ scripts/validar.js
@@ -6,21 +6,14 @@ function checkEmail(email)
     
     var regEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(!regEx.test(email)){
-        aviso.innerHTML = "Isso ainda não se parece com um endereço de e-mail D:";
+        aviso.innerHTML = "Isso ainda não se parece com um endereço de e-mail :(";
         campoEmail.style.outline = "#FF0000 auto 5px";
-        botaoCadastro.style.background = "#757575";
         botaoCadastro.style.cursor = "inherit";
         botaoCadastro.disabled = true;
         botaoCadastro.style.cursor = "not-allowed";
     }else{
-        $(botaoCadastro).hover(function(){
-            $(this).css("background-color", "#30035a")
-        }, function(){
-            $(this).css("background-color", "#46087F");
-        });
         aviso.innerHTML = "";
         campoEmail.style.outline = "#005500 auto 5px";
-        botaoCadastro.style.background = "#46087F";
         botaoCadastro.style.cursor = "pointer";
         botaoCadastro.disabled = false;
     }
@@ -33,7 +26,7 @@ function checkEmailLogin(email)
     var botaoEntrar = document.getElementById("entrar");
     var regEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(!regEx.test(email)){
-        aviso.innerHTML = "Isso ainda não se parece com um endereço de e-mail D:";
+        aviso.innerHTML = "Isso ainda não se parece com um endereço de e-mail :(";
         campoEmail.style.outline = "#FF0000 auto 5px";
        // botaoEntrar.disabled = true;
     }else{
@@ -42,5 +35,16 @@ function checkEmailLogin(email)
       //  botaoEntrar.disabled = false;
     }
 
+}
+function VerificaNumero(n) {
+    var campoNome = document.getElementById("nome");
+    var aviso = document.getElementById("aviso");
+    if ($.isNumeric(n)){
+        aviso.innerHTML = "Isso não se parece com um nome."
+        campoNome.style.outline = "#FF0000 auto 5px";
+    }else{
+        aviso.innerHTML = "";
+        campoNome.style.outline = "#005500 auto 5px";
+    }
 }
 
